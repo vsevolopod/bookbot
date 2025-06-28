0c747656c491196c5c2bf2aa2c805ad95b203cdd
vsevolopod <818vsevk@gmail.com>
2025-06-27 21:33:42 -0700

    "4: Working well"

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 ############################
 from stats import get_num_words
 from stats import count_letters
+from stats import sort_on
+from stats import dict_list
 
 def get_book_text(book):
     with open(book) as f:
@@ -12,8 +14,30 @@ def get_book_text(book):
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
-    print (get_num_words(text), f"words found in the document")
+    
+    if text.isalpha():
+        alpha = text
+        print(alpha)
+    
+    words = get_num_words(text)
+
     letters = count_letters(text)
-    print (f"count" , letters)
+    
+    listed = dict_list(letters)
+
+
+
+    listed.sort(reverse = True, key = sort_on)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at", filepath)
+    print("----------- Word Count ----------")
+    print(f"Found ",words, f"total words")
+    print("--------- Character Count -------")
+    for dicts in listed:
+        get_alphas = dicts["char"]
+        
+        if get_alphas.isalpha() == True:
+            print(dicts["char"], ":", dicts["num"])
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,25 +2,40 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-
 def count_letters(text):
-    lowercase = text.lower()
-    letters = []
-    letter_counts = {}
-    for char in lowercase:
-        if char not in letters:
-            letters.append(char)
-   # print(f"letters", letters)
-    for i in range (0, len(letters), 1):
-        cur_letter = letters[i]
-        count = 0
-        #print(cur_letter)
-        for letter in lowercase:
-            if letter == cur_letter:
-                count += 1
-                letter_counts[cur_letter[0]] = count
+    letters = {}
+    for lets in text:
+        lcd = lets.lower()
+        if lcd in letters:
+            letters[lcd] +=1
+        else:
+            letters[lcd] = 1
 
-    return letter_counts
+    return letters
 
+def dict_list(chars):
+    char_dict = {}
+    num_dict = {}
+    full_dict = {}
+    sort_list = []
+    l_keys = list(chars.keys())
+    l_vals = list(chars.values())
+    for i in range (0, len(l_keys)):
+        char_dict["char"] = l_keys[i] 
+        num_dict["num"] = l_vals[i]
+        full_dict = char_dict | num_dict
+        sort_list.append(full_dict)
+    #sort_list.sort(reverse = True, key = sort_on)   
+    return sort_list
+
+def sort_on(items):
+    return items["num"]
         
 
+    #print (l_keys[1])
+    #print (l_vals[1])
+
+
+    #print (f" silly"  , f"goosery")
+
+     
