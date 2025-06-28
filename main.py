17aa21d5d567a9f0e51c40a60fc2cb0c7ee02910
vsevolopod <151608737+vsevolopod@users.noreply.github.com>
2025-06-27 23:07:34 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+#Bookbot v1.0 
+############################
+import sys
+from stats import get_num_words
+from stats import count_letters
+from stats import sort_on
+from stats import dict_list
+try:
+    file = sys.argv[1] 
+except Exception:
+    print(f"Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+
+def get_book_text(book):
+    with open(book) as f:
+        file_contents = f.read()
+        #print("Contents inside:", file_contents)
+        return file_contents
+
+def main():
+    text = get_book_text(file)
+
+    if text.isalpha():
+        alpha = text
+        print(alpha)
+    
+    words = get_num_words(text)
+    letters = count_letters(text)
+    listed = dict_list(letters)
+
+    listed.sort(reverse = True, key = sort_on)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file}")
+    print("----------- Word Count ----------")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for dicts in listed:
+        get_alphas = dicts["char"]
+        if get_alphas.isalpha() == True:
+            print(f"{dicts["char"]}: {dicts["num"]}")
+    print("============= END ===============")
+
+main()
---
+++ stats.py
@@ -0,0 +1,41 @@
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def count_letters(text):
+    letters = {}
+    for lets in text:
+        lcd = lets.lower()
+        if lcd in letters:
+            letters[lcd] +=1
+        else:
+            letters[lcd] = 1
+
+    return letters
+
+def dict_list(chars):
+    char_dict = {}
+    num_dict = {}
+    full_dict = {}
+    sort_list = []
+    l_keys = list(chars.keys())
+    l_vals = list(chars.values())
+    for i in range (0, len(l_keys)):
+        char_dict["char"] = l_keys[i] 
+        num_dict["num"] = l_vals[i]
+        full_dict = char_dict | num_dict
+        sort_list.append(full_dict)
+    #sort_list.sort(reverse = True, key = sort_on)   
+    return sort_list
+
+def sort_on(items):
+    return items["num"]
+        
+
+    #print (l_keys[1])
+    #print (l_vals[1])
+
+
+    #print (f" silly"  , f"goosery")
+
+     
